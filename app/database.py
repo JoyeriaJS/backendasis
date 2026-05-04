4159c7cd939d9afce91c376894440f494aa0fa92
unknown <dante.h@hotmail.es>
2026-05-04 13:23:07 -0400

    cambios

--- app/database.py
+++ app/database.py
@@ -1,10 +1,6 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/asistencia"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(bind=engine)
-
-Base = declarative_base()
